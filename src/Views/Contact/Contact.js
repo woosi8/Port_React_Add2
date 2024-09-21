f006df22b54e374711d65854dc3a11e62c9aad22
woosi8 <tmfvmehek@gmail.com>
2024-09-21 21:18:17 +0900

    first merge

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -1,23 +1,23 @@
 const Contact = (props) => {
-	const handleCreate = async () => {
-		try {
-			const result = await props.request("post", "/api/go/test/create_title", {
-				content: "asdf",
-			});
-			alert(result.result);
-		} catch (error) {
-			alert(error.response.data.error);
-		}
-	};
+  const handleCreate = async () => {
+    try {
+      const result = await props.request("post", "/api/go/test/create_title", {
+        content: "asdf",
+      });
+      alert(result.result);
+    } catch (error) {
+      alert(error.response.data.error);
+    }
+  };
 
-	return (
-		<div>
-			<h1>This is Contact Page</h1>
-			<button onClick={handleCreate} type="button">
-				요청하기
-			</button>
-		</div>
-	);
+  return (
+    <div>
+      <h1>This is Contact Page</h1>
+      <button onClick={handleCreate} type="button">
+        요청하기rrrr
+      </button>
+    </div>
+  );
 };
 
 export default Contact;
